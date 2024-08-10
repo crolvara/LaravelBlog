2de5f771a464b393dfa2d1b5668b059de632c5fb
crolvara <97758851+crolvara@users.noreply.github.com>
2024-08-10 22:35:44 +0300

    update

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -8,9 +8,15 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => Job::all()
+    $jobs = Job::with('employer')->latest()->paginate(3);
+
+    return view('jobs.index', [
+        'jobs' => $jobs
     ]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
@@ -18,8 +24,20 @@ Route::get('/jobs/{id}', function ($id) {
 
 
 
-    return view('job', ['job' => $job]);
+    return view('job.show', ['job' => $job]);
 });
+
+Route::post('/jobs', function () {
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
+});
+
 
 Route::get('/contact', function () {
     return view('contact');
