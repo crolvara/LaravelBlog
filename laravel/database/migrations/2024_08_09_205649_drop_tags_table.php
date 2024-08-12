09a4c6219be9af3954962e874702036ae719ada0
crolvara <97758851+crolvara@users.noreply.github.com>
2024-08-12 23:05:34 +0300

    update

--- laravel/database/migrations/2024_08_09_205649_drop_tags_table.php
+++ laravel/database/migrations/2024_08_09_205649_drop_tags_table.php
@@ -11,22 +11,12 @@ class DropTagsTable extends Migration
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::dropIfExists('tags');
-    }
+    public function up() {}
 
     /**
      * Reverse the migrations.
      *
      * @return void
      */
-    public function down()
-    {
-        Schema::create('tags', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->timestamps();
-        });
-    }
+    public function down() {}
 }
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -7,6 +7,8 @@ Route::get('/', function () {
     return view('home');
 });
 
+
+//index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->paginate(3);
 
@@ -15,25 +17,66 @@ Route::get('/jobs', function () {
     ]);
 });
 
+//create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+//show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
-
-
-    return view('job.show', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
 });
 
+
+//store
 Route::post('/jobs', function () {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
 
     Job::create([
         'title' => request('title'),
         'salary' => request('salary'),
         'employer_id' => 1
     ]);
+
+    return redirect('/jobs');
+});
+
+
+//Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+
+//Update
+Route::patch('/jobs/{id}', function ($id) {
+    //validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+    return redirect('/jobs/' . $job->id);
+});
+
+
+//Destroy
+Route::delete('/jobs/{id}', function ($id) {
+
+    Job::findOrFail($id)->delete();
 
     return redirect('/jobs');
 });
