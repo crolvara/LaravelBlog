b47d9d754bd14584cc29b053be02fc30636e2be9
crolvara <97758851+crolvara@users.noreply.github.com>
2024-08-23 19:07:26 +0300

    update

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,6 +9,14 @@ Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
+
+// Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+Route::post('/logout', [SessionController::class, 'destroy']);
 
 //Route::controller(JobController::class)->group(function () {
 //     Route::get('/jobs', 'index');
